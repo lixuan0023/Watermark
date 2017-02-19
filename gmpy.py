0f6a855d19253969a19f714ec181a48cb432657b
lixuan0023 <lixuan0023@163.com>
2017-02-19 14:47:32 +0800

    add gmpy2

---
+++ gmpy.py
@@ -0,0 +1,13 @@
+import gmpy2
+from gmpy2 import mpz
+
+add = mpz('1')+mpz('2')
+print(add)
+sub = mpz('2')-mpz('1')
+print(sub)
+mul = mpz('2')*mpz('3')
+print(mul)
+div = mpz('2')/mpz('3')
+print(div)
+power = mpz('2')**mpz('3')
+print(power)
